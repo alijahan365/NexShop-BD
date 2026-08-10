6b9dcd3d5f05c10edf4f9d026ba1ed6faefbe7a0
Eamin <eamin3377@gmail.com>
2026-08-10 20:00:14 +0600

    signup function implemented v3

--- store/views/signup.py
+++ store/views/signup.py
@@ -32,7 +32,7 @@ class Signup(View):
             password=password
         )
 
-        error_message = None
+        error_message = self.validateCustomer(customer)
 
         if not error_message:
             customer.register()
@@ -44,3 +44,20 @@ class Signup(View):
         }
 
         return render(request, 'signup.html', data)
+
+    def validateCustomer(self, customer):
+        error_message = None
+
+        if not customer.first_name:
+            error_message = "Please Enter your First Name !!"
+
+        elif len(customer.first_name) < 3:
+            error_message = "First Name must be 3 characters long or more"
+
+        elif not customer.last_name:
+            error_message = "Please Enter your Last Name"
+
+        elif len(customer.last_name) < 3:
+            error_message = "Last Name must be 3 characters long or more"
+
+        return error_message
