6b0721660ed6a814dcb27f8055d21ee5d421adc5
Eamin <eamin3377@gmail.com>
2026-08-10 23:48:28 +0600

    final version of the login script

--- store/views/login.py
+++ store/views/login.py
@@ -1,60 +1,40 @@
-from django.shortcuts import render, redirect, HttpResponseRedirect
-from django.contrib.auth.hashers import check_password
+from django.shortcuts import render , redirect , HttpResponseRedirect
+from django.contrib.auth.hashers import  check_password
 from store.models.customer import Customer
 from django.views import View
 
 
 class Login(View):
-
     return_url = None
 
     def get(self, request):
-        Login.return_url = request.GET.get('return_url')
-
-        return render(request, 'login.html')
+        Login.return_url = request.GET.get ('return_url')
+        return render (request, 'login.html')
 
     def post(self, request):
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-
+        email = request.POST.get ('email')
+        password = request.POST.get ('password')
+        customer = Customer.get_customer_by_email (email)
         error_message = None
+        if customer:
+            flag = check_password (password, customer.password)
+            if flag:
+                request.session['customer'] = customer.id
 
-        if not email:
-            error_message = 'Please enter your email'
-
-        elif not password:
-            error_message = 'Please enter your password'
-
-        else:
-            customer = Customer.get_customer_by_email(email)
-
-            if customer:
-                flag = check_password(password, customer.password)
-
-                if flag:
-                    request.session['customer'] = customer.id
-
-                    if Login.return_url:
-                        return_url = Login.return_url
-                        Login.return_url = None
-
-                        return HttpResponseRedirect(return_url)
-
-                    return redirect('homepage')
-
+                if Login.return_url:
+                    return HttpResponseRedirect (Login.return_url)
                 else:
-                    error_message = 'Invalid email or password'
-
+                    Login.return_url = None
+                    return redirect ('homepage')
             else:
-                error_message = 'Invalid email or password'
+                error_message = 'Invalid !!'
+        else:
+            error_message = 'Invalid !!'
 
-        return render(request, 'login.html', {
-            'error': error_message,
-            'values': {
-                'email': email
-            }
-        })
-
+        print (email, password)
+        return render (request, 'login.html', {'error': error_message})
 
 def logout(request):
+    if 'customer' in request.session:
+        request.session.pop('customer')
     return redirect('login')
