f6285a2865c1a10faf6cf453d4ebfcdea579f625
Eamin <eamin@localhost>
2026-08-10 15:18:34 +0600

    feat(auth): add customer registration method

--- store/models/customer.py
+++ store/models/customer.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -8,5 +7,21 @@ class Customer(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=500)
 
+    #to save the data
     def register(self):
         self.save()
+
+
+    @staticmethod
+    def get_customer_by_email(email):
+        try:
+            return Customer.objects.get(email= email)
+        except:
+            return False
+
+
+    def isExists(self):
+        if Customer.objects.filter(email = self.email):
+            return True
+
+        return False
