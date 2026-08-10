8c16906e75ea341d437565243ef4b45b96c3ca90
arafatai388 <arafat.cseai@gmail.com>
2026-08-10 09:57:57 +0600

    Add product model

---
+++ store/models/product.py
@@ -0,0 +1,35 @@
+from django.db import models
+from .category import Category
+from django.db.models import Q
+
+class Products(models.Model):
+    name = models.CharField(max_length=255)
+    price= models.IntegerField(default=0)
+    category= models.ForeignKey(Category,on_delete=models.CASCADE,default=1 )
+    description= models.CharField(max_length=250, default='', blank=True, null= True)
+    image= models.ImageField(upload_to='uploads/products/')
+
+    @staticmethod
+    def get_products_by_id(ids):
+        return Products.objects.select_related('category').filter(id__in=ids)
+
+    @staticmethod
+    def get_all_products():
+        return Products.objects.select_related('category').all()
+
+    @staticmethod
+    def get_all_products_by_categoryid(category_id):
+        if category_id:
+            return Products.objects.select_related('category').filter(category=category_id)
+        else:
+            return Products.get_all_products()
+
+    @staticmethod
+    def search_products(query):
+        if query:
+            return Products.objects.select_related('category').filter(
+                Q(name__icontains=query) | 
+                Q(description__icontains=query) | 
+                Q(category__name__icontains=query)
+            )
+        return Products.get_all_products()
